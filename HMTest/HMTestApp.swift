8c4216025bd6213bf3b31460a1fdcf31ee21790e
Ardak <ardaktursunbayev4@gmail.com>
2023-10-23 19:07:09 +0600

    Initial Commit

---
+++ HMTest/HMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMTestApp.swift
+//  HMTest
+//
+//  Created by Ardak Tursunbayev on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
